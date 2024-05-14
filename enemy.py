9f3c8f16b755c5b6dc0238d18a856f508cab136c
Flowzitto <quentin.hemeryck@std.heh.be>
2024-05-14 23:47:11 +0200

    Ajout du suivi des ennemies

--- enemy.py
+++ enemy.py
@@ -115,16 +115,15 @@ class Enemy:
             self.__y += move_y
 
     def move_towards_player(self, player_x, player_y):
-        dx = player_x - self.__x
-        dy = player_y - self.__y
-        distance = max(abs(dx), abs(dy))
+        dx = player_x - self.x
+        dy = player_y - self.y
+        distance = (dx ** 2 + dy ** 2) ** 0.5  # Calculer la distance euclidienne
         if distance != 0:
-            step_x = dx / distance * self.__speed
-            step_y = dy / distance * self.__speed
-            if self.check_oob(self.__x + step_x, self.__y + step_y):
-                self.__x += step_x
-                self.__y += step_y
-
+            step_x = dx / distance * self.speed
+            step_y = dy / distance * self.speed
+            if self.check_oob(self.x + step_x, self.y + step_y):
+                self.x += step_x
+                self.y += step_y
 class Lizard(Enemy):
     def __init__(self, speed, screen_width=679, screen_height=676):
         lizard_frames = ["assets/big_demon_run_anim_f0.png", "assets/big_demon_run_anim_f1.png",
